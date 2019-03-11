2d5cc4d6dd85cef2b8412df882c02aa154baecde
nuhundred@gmail.com <nuhundred@gmail.com>
2019-03-11 10:26:26 +0700

    add google map api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {keyConfig} from './config.js'
+import {Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+}
+
 
 class App extends Component {
   render() {
@@ -8,21 +16,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Google Map with React</h1>
         </header>
+        <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+
+        </InfoWindow>
+      </Map>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: (keyConfig.googleApiKey)
+})(App)
